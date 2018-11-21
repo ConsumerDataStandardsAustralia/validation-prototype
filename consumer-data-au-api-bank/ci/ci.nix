44ed9e9eed16178b82b9e1d9017e3f2271acd40c
Andrew McMiddlin <andrew@ajmccluskey.com>
2018-11-21 21:20:09 +1000

    Add ci files for hydra
    Hopefully this works first time!

---
+++ consumer-data-au-api-bank/ci/ci.nix
@@ -0,0 +1,30 @@
+{ supportedSystems ? ["x86_64-linux"]
+, supportedCompilers ? [ "ghc844" ]
+}:
+
+with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
+
+let
+  pkgs = import <nixpkgs> {};
+
+  configurations =
+    pkgs.lib.listToAttrs (
+      pkgs.lib.concatMap (compiler:
+        pkgs.lib.concatMap (system:
+          [{name = "haskell.packages." + compiler + ".consumer-data-au-api-bank." + system ; value = {inherit compiler system;};}]
+        ) supportedSystems
+      ) supportedCompilers
+    );
+
+  jobs =
+      pkgs.lib.mapAttrs (name: configuration:
+          let
+            compiler = configuration.compiler;
+            system = configuration.system;
+            nixpkgs = { pkgs = pkgsFor system; };
+            consumer-data-au-api-bank = import ../default.nix { inherit nixpkgs compiler; };
+          in
+            consumer-data-au-api-bank
+      ) configurations;
+in
+jobs
