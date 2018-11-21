8ff37b9cfe381e04c575d21b09d131950bfd98f8
Andrew McCluskey <andrew@ajmccluskey.com>
2018-11-21 12:09:18 +1000

    Move to overrides that can be composed using `composeExtensions`
    This will let us compose our overrides as we introduce other packages that
    depend on this one and may then have their own overrides.

--- consumer-data-au-api-types/nix/haskellPackages.nix
+++ consumer-data-au-api-types/nix/haskellPackages.nix
@@ -7,23 +7,12 @@ let
     then pkgs.haskellPackages
     else pkgs.haskell.packages.${compiler};
 
-  upstreamSources = {
-    servant-waargonaut = (import ./servant-waargonaut.nix) { inherit nixpkgs; };
-  };
-  
-  upstreamOverrides = {
-    servant-waargonaut = (import "${upstreamSources.servant-waargonaut}/servant-waargonaut-overrides.nix");
-  };
-
-  obOverrides = pkgs: (self: super: {
-    servant-waargonaut = self.callPackage (import "${upstreamSources.servant-waargonaut}/servant-waargonaut.nix") {};
-  });
+  obOverrides = import ./consumer-data-au-api-types-overrides.nix pkgs;
 
   obHaskellPackages = haskellPackages.override (old: {
-    overrides = pkgs.lib.foldr 
-      (x: acc: pkgs.lib.composeExtensions acc (x { inherit pkgs; }))
-      (old.overrides or (_: _: {}))
-      [upstreamOverrides.servant-waargonaut obOverrides];
+    overrides = pkgs.lib.composeExtensions
+      (old.overrides or (_:_: {}))
+      obOverrides;
   });
 in
   obHaskellPackages
