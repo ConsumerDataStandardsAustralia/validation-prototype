43e124ff78eb7da57effcfea8a5ee72d43273b31
Ben Kolera <ben.kolera@gmail.com>
2018-11-23 17:05:53 +1000

    Irresistible pedantry.

--- consumer-data-au-lambdabank/src/Web/ConsumerData/Au/LambdaBank/Server/Banking.hs
+++ consumer-data-au-lambdabank/src/Web/ConsumerData/Au/LambdaBank/Server/Banking.hs
@@ -44,25 +44,49 @@ bankingServer = genericServerT BankingApi
       , _accountsBalancesPost = bankPaginatedResponse
         testBalances
         (fakePaginator Nothing (links^.bankingLinks.bankingAccountsLinks.accountsBalancesGet))
-      , _accountsTransactionsGet = \pMay -> bankPaginatedResponse testAccountsTransactions (fakePaginator pMay (const $ (links^.bankingLinks.bankingAccountsLinks.accountsTransactionsGet) pMay))
-      , _accountsTransactionsPost = bankPaginatedResponse testAccountsTransactions (fakePaginator Nothing (const $ links^.bankingLinks.bankingAccountsLinks.accountsTransactionsPost))
-      , _accountsDirectDebitsGet = \pMay -> bankPaginatedResponse testDirectDebitAuthorisations (fakePaginator pMay (links^.bankingLinks.bankingAccountsLinks.accountsDirectDebitsGet))
-      , _accountsDirectDebitsPost = bankPaginatedResponse testDirectDebitAuthorisations (fakePaginator Nothing (const $ links^.bankingLinks.bankingAccountsLinks.accountsDirectDebitsPost))
-      , _accountsById = \accountId -> genericServerT AccountApi
-        { _accountGet                = bankStandardResponse testAccountDetail (links^.bankingLinks.bankingAccountsLinks.accountsByIdLinks.to ($accountId).accountGet)
-        , _accountTransactionsGet    = bankPaginatedResponse testAccountTransactions (fakePaginator Nothing (const $ links^.bankingLinks.bankingAccountsLinks.accountsByIdLinks.to ($accountId).accountTransactionsGet))
-        , _accountTransactionByIdGet = \transactionId -> bankStandardResponse testAccountTransactionDetail ((links^.bankingLinks.bankingAccountsLinks.accountsByIdLinks.to ($accountId).accountTransactionByIdGet) transactionId)
-        , _accountDirectDebitsGet    = bankPaginatedResponse
+      , _accountsTransactionsGet = \pMay -> bankPaginatedResponse
+        testAccountsTransactions
+        (fakePaginator pMay (const $ (links^.bankingLinks.bankingAccountsLinks.accountsTransactionsGet) pMay))
+      , _accountsTransactionsPost = bankPaginatedResponse
+        testAccountsTransactions
+        (fakePaginator Nothing (const $ links^.bankingLinks.bankingAccountsLinks.accountsTransactionsPost))
+      , _accountsDirectDebitsGet = \pMay -> bankPaginatedResponse
         testDirectDebitAuthorisations
-        (fakePaginator Nothing (const $ links^.bankingLinks.bankingAccountsLinks.accountsByIdLinks.to ($accountId).accountDirectDebitsGet))
+        (fakePaginator pMay (links^.bankingLinks.bankingAccountsLinks.accountsDirectDebitsGet))
+      , _accountsDirectDebitsPost = bankPaginatedResponse
+        testDirectDebitAuthorisations
+        (fakePaginator Nothing (const $ links^.bankingLinks.bankingAccountsLinks.accountsDirectDebitsPost))
+      , _accountsById = \accountId -> genericServerT AccountApi
+        { _accountGet                = bankStandardResponse
+          testAccountDetail
+          (links^.bankingLinks.bankingAccountsLinks.accountsByIdLinks.to ($accountId).accountGet)
+        , _accountTransactionsGet    = bankPaginatedResponse
+          testAccountTransactions
+          (fakePaginator Nothing
+           (const $ links^.bankingLinks.bankingAccountsLinks.accountsByIdLinks.to ($accountId).accountTransactionsGet))
+        , _accountTransactionByIdGet = \transactionId -> bankStandardResponse
+          testAccountTransactionDetail
+          ((links^.bankingLinks.bankingAccountsLinks.accountsByIdLinks.to ($accountId).accountTransactionByIdGet) transactionId)
+        , _accountDirectDebitsGet    = bankPaginatedResponse
+          testDirectDebitAuthorisations
+          (fakePaginator Nothing
+           (const $ links^.bankingLinks.bankingAccountsLinks.accountsByIdLinks.to ($accountId).accountDirectDebitsGet))
         }
       }
     , _bankingPayees = genericServerT PayeesApi
-      { _payeesGet = bankPaginatedResponse testPayees (fakePaginator Nothing (const $ links^.bankingLinks.bankingPayeesLinks.payeesGet))
-      , _payeesByIdGet = \_payeeId -> bankStandardResponse testPayeeDetail (links^.bankingLinks.bankingPayeesLinks.payeesByIdGet $ _payeeId)
+      { _payeesGet = bankPaginatedResponse
+        testPayees
+        (fakePaginator Nothing (const $ links^.bankingLinks.bankingPayeesLinks.payeesGet))
+      , _payeesByIdGet = \_payeeId -> bankStandardResponse
+        testPayeeDetail
+        (links^.bankingLinks.bankingPayeesLinks.payeesByIdGet $ _payeeId)
       }
     , _bankingProducts = genericServerT ProductsApi
-      { _productsGet = bankPaginatedResponse [testProduct] (fakePaginator Nothing (const $ links^.bankingLinks.bankingProductsLinks.productsGet))
-      , _productsByIdGet = \_productId -> bankStandardResponse testProductDetail (links^.bankingLinks.bankingProductsLinks.productsByIdGet $ _productId)
+      { _productsGet = bankPaginatedResponse
+        [testProduct]
+        (fakePaginator Nothing (const $ links^.bankingLinks.bankingProductsLinks.productsGet))
+      , _productsByIdGet = \_productId -> bankStandardResponse
+        testProductDetail
+        (links^.bankingLinks.bankingProductsLinks.productsByIdGet $ _productId)
       }
     }
