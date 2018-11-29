bb56d8d869c9cacee3a0cc6e19f243879417e2f2
Ben Kolera <ben.kolera@gmail.com>
2018-11-30 06:23:12 +1000

    more ignores. This was a mistake. We will need to split up api types to cull out some ghcjs deps I think

--- consumer-data-au-mock-consumer/nix/consumer-data-au-mock-consumer-overrides.nix
+++ consumer-data-au-mock-consumer/nix/consumer-data-au-mock-consumer-overrides.nix
@@ -13,6 +13,29 @@ let
     iproute = pkgs.haskell.lib.dontCheck super.iproute;
     mockery = pkgs.haskell.lib.dontCheck super.mockery;
     silently = pkgs.haskell.lib.dontCheck super.silently;
+    bifunctors = pkgs.haskell.lib.dontCheck super.bifunctors;
+    unix-time = pkgs.haskell.lib.dontCheck super.unix-time;
+    markdown-unlit = pkgs.haskell.lib.dontCheck super.markdown-unlit;
+    http-date = pkgs.haskell.lib.dontCheck super.http-date;
+    unliftio = pkgs.haskell.lib.dontCheck super.unliftio;
+    hw-hspec-hedgehog = pkgs.haskell.lib.dontCheck super.hw-hspec-hedgehog;
+    bsb-hw-chunked = pkgs.haskell.lib.dontCheck super.bsb-hw-chunked;
+    bsb-http-chunked = pkgs.haskell.lib.dontCheck super.bsb-http-chunked;
+    bits-extra = pkgs.haskell.lib.dontCheck super.bits-extra;
+    zippers = pkgs.haskell.lib.dontCheck super.zippers;
+    monad-par = pkgs.haskell.lib.dontCheck super.monad-par;
+    hw-prim = pkgs.haskell.lib.dontCheck super.hw-prim;
+    hw-rankselect-base = pkgs.haskell.lib.dontCheck super.hw-rankselect-base;
+    hw-rankselect = pkgs.haskell.lib.dontCheck super.hw-rankselect;
+    conduit = pkgs.haskell.lib.dontCheck super.conduit;
+    aeson-diff = pkgs.haskell.lib.dontCheck super.aeson-diff;
+    http2 = pkgs.haskell.lib.dontCheck super.http2;
+    yaml = pkgs.haskell.lib.dontCheck super.yaml;
+    wai-extra = pkgs.haskell.lib.dontCheck super.wai-extra;
+    hpack = pkgs.haskell.lib.dontCheck super.hpack;
+    wai-app-static = pkgs.haskell.lib.dontCheck super.wai-app-static;
+
+
   };
 
   composedOverrides =
