95f0e7c35ca717275f6ea8df1edc07c942589d4e
Andrew McCluskey <andrew@ajmccluskey.com>
2018-12-04 11:40:51 +1000

    Stop exporting things with no exports.
    Causes a warning. Export them when we need/use them.

--- consumer-data-au-api-types/src/Web/ConsumerData/Au/Api/Types/Auth.hs
+++ consumer-data-au-api-types/src/Web/ConsumerData/Au/Api/Types/Auth.hs
@@ -1,18 +1,13 @@
 module Web.ConsumerData.Au.Api.Types.Auth
-  ( module Web.ConsumerData.Au.Api.Types.Auth
-  , module AuthApi
+  ( module AuthApi
   , module AuthorisationRequest
   , module Common
   , module Error
-  , module Registration
   , module Response
-  , module TokenRequest
   ) where
 
 import Web.ConsumerData.Au.Api.Types.Auth.AuthApi              as AuthApi
 import Web.ConsumerData.Au.Api.Types.Auth.AuthorisationRequest as AuthorisationRequest
 import Web.ConsumerData.Au.Api.Types.Auth.Common               as Common
 import Web.ConsumerData.Au.Api.Types.Auth.Error                as Error
-import Web.ConsumerData.Au.Api.Types.Auth.Registration         as Registration
 import Web.ConsumerData.Au.Api.Types.Auth.Response             as Response
-import Web.ConsumerData.Au.Api.Types.Auth.TokenRequest         as TokenRequest
