6ab1eebc6ddfb01b7bd1f0aec09759ccf98affa2
Erzsebet Nemeth <nemeth.erzsebet.79@gmail.com>
2018-12-12 11:12:49 +1000

    Add Accounts Gen

--- consumer-data-au-api-types/tests/Web/ConsumerData/Au/Api/Types/Banking/AccountsGens.hs
+++ consumer-data-au-api-types/tests/Web/ConsumerData/Au/Api/Types/Banking/AccountsGens.hs
@@ -1,10 +1,44 @@
 module Web.ConsumerData.Au.Api.Types.Banking.AccountsGens where
 
-import Hedgehog
+import           Data.Text.Gens (textGen)
+import           Hedgehog
+import qualified Hedgehog.Gen   as Gen
+import qualified Hedgehog.Range as Range
 
 import Web.ConsumerData.Au.Api.Types
 import Web.ConsumerData.Au.Api.Types.Data.CommonFieldTypesGens
+    (asciiStringGen, currencyAmountGen)
 
 
 accountIdGen :: Gen AccountId
 accountIdGen = AccountId <$> asciiStringGen
+
+accountGen :: Gen Account
+accountGen = Account
+  <$> accountIdGen
+  <*> textGen
+  <*> Gen.maybe textGen
+  <*> maskedAccountNumberGen
+  <*> Gen.maybe productCategoryGen
+  <*> textGen
+  <*> balanceGen
+
+maskedAccountNumberGen :: Gen MaskedAccountNumber
+maskedAccountNumberGen = MaskedAccountNumber <$> textGen
+
+productCategoryGen :: Gen ProductCategory
+productCategoryGen = Gen.enumBounded
+
+balanceGen :: Gen Balance
+balanceGen = Gen.choice
+  [ Deposits
+      <$> currencyAmountGen
+      <*> currencyAmountGen
+  , Lending
+      <$> currencyAmountGen
+      <*> currencyAmountGen
+      <*> currencyAmountGen
+      <*> Gen.maybe currencyAmountGen
+  , Purses
+      <$> Gen.list (Range.linear 0 3) currencyAmountGen
+  ]
