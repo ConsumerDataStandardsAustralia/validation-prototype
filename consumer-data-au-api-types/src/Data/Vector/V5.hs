996b30e1664e8e5fea9c73f35b0b3c5e7620be86
Tony Morris <tmorris@tmorris.net>
2019-01-15 15:03:58 +1000

    V5/V6 enhancements

--- consumer-data-au-api-types/src/Data/Vector/V5.hs
+++ consumer-data-au-api-types/src/Data/Vector/V5.hs
@@ -2,15 +2,26 @@
 {-# LANGUAGE DeriveFoldable    #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Data.Vector.V5 where
 
+import Control.Applicative(liftA2)
 import Control.Lens
 import Control.Monad.Except (throwError)
+import Control.Monad.Zip(MonadZip(mzipWith))
+import Data.Functor.Classes
 import Data.Digit.Char (charDecimal)
 import Data.Digit.Decimal
+import Data.Functor.Apply(Apply)
+import Data.Functor.Bind(Bind((>>-)))
+import qualified Data.Functor.Apply as Apply((<.>))
 import Data.Functor.Contravariant (contramap, (>$<))
 import Data.Foldable (toList)
+import Data.Maybe(fromMaybe)
+import Data.Semigroup.Foldable(Foldable1(foldMap1))
 import qualified Data.Text as Text
 
 import Waargonaut.Decode (Decoder)
@@ -23,15 +34,122 @@ data V5 a =
   V5 a a a a a
   deriving (Eq, Ord, Functor, Foldable, Traversable)
 
+instance Eq1 V5 where
+  liftEq f (V5 a1 a2 a3 a4 a5) (V5 b1 b2 b3 b4 b5) =
+    f a1 b1 && f a2 b2 && f a3 b3 && f a4 b4 && f a5 b5
+
+instance Ord1 V5 where
+  liftCompare f (V5 a1 a2 a3 a4 a5) (V5 b1 b2 b3 b4 b5) =
+    f a1 b1 `mappend` f a2 b2 `mappend` f a3 b3 `mappend` f a4 b4 `mappend` f a5 b5
+
+instance Show1 V5 where
+  liftShowsPrec f k d (V5 a1 a2 a3 a4 a5) =
+    liftShowsPrec f k d [a1, a2, a3, a4, a5]
+
+instance Apply V5 where
+  V5 f1 f2 f3 f4 f5 <.> V5 a1 a2 a3 a4 a5 =
+    V5 (f1 a1) (f2 a2) (f3 a3) (f4 a4) (f5 a5)
+
+instance Applicative V5 where
+  (<*>) =
+    (Apply.<.>)
+  pure a =
+    V5 a a a a a
+
+instance Bind V5 where
+  V5 a1 a2 a3 a4 a5 >>- f =
+    let V5 a1' _ _ _ _ =
+          f a1
+        V5 _ a2' _ _ _ =
+          f a2
+        V5 _ _ a3' _ _ =
+          f a3
+        V5 _ _ _ a4' _ =
+          f a4
+        V5 _ _ _ _ a5' =
+          f a5
+    in  V5 a1' a2' a3' a4' a5'
+
+instance Monad V5 where
+  (>>=) =
+    (>>-)
+  return =
+    pure
+
 instance Show a => Show (V5 a) where
   showsPrec _ = showList . toList
 
-v5List :: Prism' [a] (V5 a)
-v5List = prism toList v5FromList
+type instance Index (V5 a) = Int
+type instance IxValue (V5 a) = a
 
-v5FromList :: [a] -> Either [a] (V5 a)
-v5FromList [a,b,c,d,e] = Right (V5 a b c d e)
-v5FromList as = Left as
+instance At (V5 a) where
+  at n f v5@(V5 a1 a2 a3 a4 a5) =
+    let f' = f . Just
+    in  case n of
+          0 ->
+            fmap (\a1' -> V5 (fromMaybe a1 a1') a2 a3 a4 a5) (f' a1)
+          1 ->
+            fmap (\a2' -> V5 a1 (fromMaybe a2 a2') a3 a4 a5) (f' a2)
+          2 ->
+            fmap (\a3' -> V5 a1 a2 (fromMaybe a3 a3') a4 a5) (f' a3)
+          3 ->
+            fmap (\a4' -> V5 a1 a2 a3 (fromMaybe a4 a4') a5) (f' a4)
+          4 ->
+            fmap (\a5' -> V5 a1 a2 a3 a4 (fromMaybe a5 a5')) (f' a5)
+          _ ->
+            v5 <$ f Nothing
+
+instance Ixed (V5 a) where
+  ix n =
+    at n . traverse
+
+instance Each (V5 a) (V5 b) a b where
+  each =
+    traverse
+
+instance Reversing (V5 a) where
+  reversing (V5 a1 a2 a3 a4 a5) =
+    V5 a5 a4 a3 a2 a1
+
+
+instance MonadZip V5 where
+  mzipWith =
+    liftA2
+
+instance Field1 (V5 a) (V5 a) a a where
+  _1 f (V5 a1 a2 a3 a4 a5) =
+    (\a1' -> V5 a1' a2 a3 a4 a5) <$> f a1
+
+instance Field2 (V5 a) (V5 a) a a where
+  _2 f (V5 a1 a2 a3 a4 a5) =
+    (\a2' -> V5 a1 a2' a3 a4 a5) <$> f a2
+
+instance Field3 (V5 a) (V5 a) a a where
+  _3 f (V5 a1 a2 a3 a4 a5) =
+    (\a3' -> V5 a1 a2 a3' a4 a5) <$> f a3
+
+instance Field4 (V5 a) (V5 a) a a where
+  _4 f (V5 a1 a2 a3 a4 a5) =
+    (\a4' -> V5 a1 a2 a3 a4' a5) <$> f a4
+
+instance Field5 (V5 a) (V5 a) a a where
+  _5 f (V5 a1 a2 a3 a4 a5) =
+    (\a5' -> V5 a1 a2 a3 a4 a5') <$> f a5
+
+instance Foldable1 V5 where
+  foldMap1 f (V5 a1 a2 a3 a4 a5) =
+    f a1 <> f a2 <> f a3 <> f a4 <> f a5
+
+instance Traversable1 V5 where
+  traverse1 f (V5 a1 a2 a3 a4 a5) =
+    V5 <$> f a1 Apply.<.> f a2 Apply.<.> f a3 Apply.<.> f a4 Apply.<.> f a5
+
+v5List :: Prism' [a] (V5 a)
+v5List = prism' toList v5FromList
+
+v5FromList :: [a] -> Maybe (V5 a)
+v5FromList [a,b,c,d,e] = Just (V5 a b c d e)
+v5FromList _ = Nothing
 
 v5CharDecoder :: Monad f => Decoder f (V5 Char)
 v5CharDecoder =
--- consumer-data-au-api-types/src/Data/Vector/V6.hs
+++ consumer-data-au-api-types/src/Data/Vector/V6.hs
@@ -2,15 +2,26 @@
 {-# LANGUAGE DeriveFoldable    #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Data.Vector.V6 where
 
+import Control.Applicative(liftA2)
 import Control.Lens
 import Control.Monad.Except (throwError)
+import Control.Monad.Zip(MonadZip(mzipWith))
 import Data.Digit.Char (charDecimal)
 import Data.Digit.Decimal
+import Data.Functor.Apply(Apply)
+import Data.Functor.Bind(Bind((>>-)))
+import qualified Data.Functor.Apply as Apply((<.>))
 import Data.Functor.Contravariant (contramap, (>$<))
 import Data.Foldable (toList)
+import Data.Functor.Classes
+import Data.Maybe(fromMaybe)
+import Data.Semigroup.Foldable
 import qualified Data.Text as Text
 
 import Waargonaut.Decode (Decoder)
@@ -23,15 +34,129 @@ data V6 a =
   V6 a a a a a a
   deriving (Eq, Ord, Functor, Foldable, Traversable)
 
+instance Eq1 V6 where
+  liftEq f (V6 a1 a2 a3 a4 a5 a6) (V6 b1 b2 b3 b4 b5 b6) =
+    f a1 b1 && f a2 b2 && f a3 b3 && f a4 b4 && f a5 b5 && f a6 b6
+
+instance Ord1 V6 where
+  liftCompare f (V6 a1 a2 a3 a4 a5 a6) (V6 b1 b2 b3 b4 b5 b6) =
+    f a1 b1 `mappend` f a2 b2 `mappend` f a3 b3 `mappend` f a4 b4 `mappend` f a5 b5 `mappend` f a6 b6
+
+instance Show1 V6 where
+  liftShowsPrec f k d (V6 a1 a2 a3 a4 a5 a6) =
+    liftShowsPrec f k d [a1, a2, a3, a4, a5, a6]
+
+instance Apply V6 where
+  V6 f1 f2 f3 f4 f5 f6 <.> V6 a1 a2 a3 a4 a5 a6 =
+    V6 (f1 a1) (f2 a2) (f3 a3) (f4 a4) (f5 a5) (f6 a6)
+
+instance Applicative V6 where
+  (<*>) =
+    (Apply.<.>)
+  pure a =
+    V6 a a a a a a
+
+instance Bind V6 where
+  V6 a1 a2 a3 a4 a5 a6 >>- f =
+    let V6 a1' _ _ _ _ _ =
+          f a1
+        V6 _ a2' _ _ _ _ =
+          f a2
+        V6 _ _ a3' _ _ _ =
+          f a3
+        V6 _ _ _ a4' _ _ =
+          f a4
+        V6 _ _ _ _ a5' _ =
+          f a5
+        V6 _ _ _ _ _ a6' =
+          f a6
+    in  V6 a1' a2' a3' a4' a5' a6'
+
+instance Monad V6 where
+  (>>=) =
+    (>>-)
+  return =
+    pure
+
 instance Show a => Show (V6 a) where
   showsPrec _ = showList . toList
 
-v6List :: Prism' [a] (V6 a)
-v6List = prism toList v6FromList
+type instance Index (V6 a) = Int
+type instance IxValue (V6 a) = a
 
-v6FromList :: [a] -> Either [a] (V6 a)
-v6FromList [a,b,c,d,e,f] = Right (V6 a b c d e f)
-v6FromList as = Left as
+instance At (V6 a) where
+  at n f v6@(V6 a1 a2 a3 a4 a5 a6) =
+    let f' = f . Just
+    in  case n of
+          0 ->
+            fmap (\a1' -> V6 (fromMaybe a1 a1') a2 a3 a4 a5 a6) (f' a1)
+          1 ->
+            fmap (\a2' -> V6 a1 (fromMaybe a2 a2') a3 a4 a5 a6) (f' a2)
+          2 ->
+            fmap (\a3' -> V6 a1 a2 (fromMaybe a3 a3') a4 a5 a6) (f' a3)
+          3 ->
+            fmap (\a4' -> V6 a1 a2 a3 (fromMaybe a4 a4') a5 a6) (f' a4)
+          4 ->
+            fmap (\a5' -> V6 a1 a2 a3 a4 (fromMaybe a5 a5') a6) (f' a5)
+          5 ->
+            fmap (\a6' -> V6 a1 a2 a3 a4 a5 (fromMaybe a6 a6')) (f' a6)
+          _ ->
+            v6 <$ f Nothing
+
+instance Ixed (V6 a) where
+  ix n =
+    at n . traverse
+
+instance Each (V6 a) (V6 b) a b where
+  each =
+    traverse
+
+instance Reversing (V6 a) where
+  reversing (V6 a1 a2 a3 a4 a5 a6) =
+    V6 a6 a5 a4 a3 a2 a1
+
+instance MonadZip V6 where
+  mzipWith =
+    liftA2
+
+instance Field1 (V6 a) (V6 a) a a where
+  _1 f (V6 a1 a2 a3 a4 a5 a6) =
+    (\a1' -> V6 a1' a2 a3 a4 a5 a6) <$> f a1
+
+instance Field2 (V6 a) (V6 a) a a where
+  _2 f (V6 a1 a2 a3 a4 a5 a6) =
+    (\a2' -> V6 a1 a2' a3 a4 a5 a6) <$> f a2
+
+instance Field3 (V6 a) (V6 a) a a where
+  _3 f (V6 a1 a2 a3 a4 a5 a6) =
+    (\a3' -> V6 a1 a2 a3' a4 a5 a6) <$> f a3
+
+instance Field4 (V6 a) (V6 a) a a where
+  _4 f (V6 a1 a2 a3 a4 a5 a6) =
+    (\a4' -> V6 a1 a2 a3 a4' a5 a6) <$> f a4
+
+instance Field5 (V6 a) (V6 a) a a where
+  _5 f (V6 a1 a2 a3 a4 a5 a6) =
+    (\a5' -> V6 a1 a2 a3 a4 a5' a6) <$> f a5
+
+instance Field6 (V6 a) (V6 a) a a where
+  _6 f (V6 a1 a2 a3 a4 a5 a6) =
+    (\a6' -> V6 a1 a2 a3 a4 a5 a6') <$> f a6
+
+instance Foldable1 V6 where
+  foldMap1 f (V6 a1 a2 a3 a4 a5 a6) =
+    f a1 <> f a2 <> f a3 <> f a4 <> f a5 <> f a6
+
+instance Traversable1 V6 where
+  traverse1 f (V6 a1 a2 a3 a4 a5 a6) =
+    V6 <$> f a1 Apply.<.> f a2 Apply.<.> f a3 Apply.<.> f a4 Apply.<.> f a5 Apply.<.> f a6
+
+v6List :: Prism' [a] (V6 a)
+v6List = prism' toList v6FromList
+
+v6FromList :: [a] -> Maybe (V6 a)
+v6FromList [a,b,c,d,e,f] = Just (V6 a b c d e f)
+v6FromList _ = Nothing
 
 v6CharDecoder :: Monad f => Decoder f (V6 Char)
 v6CharDecoder =
